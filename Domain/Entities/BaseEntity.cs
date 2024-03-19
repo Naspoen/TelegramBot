082150f6acc65c51e87182aae439ee165450e3cb
nsp99 <kovtunenkomarat@gmail.com>
2024-03-19 10:04:53 +0200

    Initial commit

---
+++ Domain/Entities/BaseEntity.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    /// <summary>
+    /// Базовый класс для всех сущностей
+    /// </summary>
+    public abstract class BaseEntity
+    {
+        /// <summary>
+        /// Уникальный идентификатор сущностей
+        /// </summary>
+        protected Guid Id { get; set; }
+        /// <summary>
+        /// Дата создания сущности
+        /// </summary>
+        protected DateTime CreatedDate { get; set; }
+        /// <summary>
+        /// Конструктор класса BaseEntity
+        /// </summary>
+        /// <param name="id">ID</param>
+        /// <param name="createdDate">Дата создания</param>
+        protected BaseEntity(Guid id, DateTime createdDate)
+        {
+            Id = id;
+            CreatedDate = createdDate;
+        }
+        /// <summary>
+        /// Метод для сравнения двух сущностей по ID
+        /// </summary>
+        public override bool Equals(object? obj)
+        {
+            if (obj == null) 
+                return false;
+            if (obj is not BaseEntity entity)
+                return false;
+            if (Id != ((BaseEntity)obj).Id)
+                return false;
+            return true;
+        }
+        /// <summary>
+        /// TODO: Домашнее задание (понять почему надо переписать). Сделать*
+        /// </summary>
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+    }
+}
---
+++ Domain/Entities/Person.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    /// <summary>
+    ///  Основная сущность персоны
+    /// </summary>
+    public class Person : BaseEntity
+    {
+        // TODO: (До четверга) Максимально полно описать сущность персоны (др, возраст, пол, имя). Все поля добавляются через конструктор.
+        public DateTime DateOfBirth { get; set; }
+        public int Age => CalculateAge();
+        /// <summary>
+        /// Пол (true - мужской, false - женский)
+        /// </summary>
+        public bool Gender { get; set; }
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public string Telegram { get; set; }
+
+        public Person(Guid id, DateTime createdDate, DateTime dateOfBirth, bool gender, string firstName, string secondName, string telegram) : base(id, createdDate)
+        {
+            DateOfBirth = dateOfBirth;
+            Gender = gender;
+            FirstName = firstName;
+            SecondName = secondName;
+            Telegram = telegram;
+        }
+        /// <summary>
+        /// Метод для получения возраста
+        /// </summary>
+        private int CalculateAge()
+        {
+            DateTime now = DateTime.UtcNow;
+            int age = now.Year - DateOfBirth.Year;
+            if (now < DateOfBirth.AddYears(age)) 
+                age--;
+            return age;
+        }
+    }
+}
